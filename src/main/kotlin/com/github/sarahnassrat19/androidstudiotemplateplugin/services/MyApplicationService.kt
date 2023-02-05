3e8abc8a61b044370323728f89cfb082f02d8245
GitHub Action <action@github.com>
2023-02-05 10:51:41 +0000

    Template cleanup

--- src/main/kotlin/com/github/sarahnassrat19/androidstudiotemplateplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/sarahnassrat19/androidstudiotemplateplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.sarahnassrat19.androidstudiotemplateplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.sarahnassrat19.androidstudiotemplateplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/sarahnassrat19/androidstudiotemplateplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/sarahnassrat19/androidstudiotemplateplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.sarahnassrat19.androidstudiotemplateplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.sarahnassrat19.androidstudiotemplateplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
